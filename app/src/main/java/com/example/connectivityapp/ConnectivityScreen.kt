3c32e6ccb38666e0839f38307eb55ad0684e009a
purboyndra <purboindra@gmail.com>
2024-11-25 06:41:53 +0700

    feat: ConnectivityScreen

---
+++ app/src/main/java/com/example/connectivityapp/ConnectivityScreen.kt
@@ -0,0 +1,42 @@
+package com.example.connectivityapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.launch
+
+@Composable
+fun ConnectivityScreen(modifier: Modifier) {
+    
+    val context = LocalContext.current
+    
+    val connectionType = getConnectionType(context)
+    
+    val coroutineScope = rememberCoroutineScope()
+    
+    var signalStrength by remember {
+        mutableIntStateOf(-1)
+    }
+    
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            val signalWifiStrength = getWifiSignalStrength(context)
+            signalStrength = signalWifiStrength
+        }
+    }
+    
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+        Text("You're currently online with: $connectionType ")
+        Text("Your Signal strength: $signalStrength ")
+    }
+}
--- app/src/main/java/com/example/connectivityapp/MainActivity.kt
+++ app/src/main/java/com/example/connectivityapp/MainActivity.kt
@@ -6,21 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.connectivityapp.ui.theme.ConnectivityAppTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,47 +18,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConnectivityAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding()
+                        .safeContentPadding()
+                ) { innerPadding ->
+                    ConnectivityScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    
-    val context = LocalContext.current
-    
-    val connectionType = getConnectionType(context)
-    
-    val coroutineScope = rememberCoroutineScope()
-    
-    var signalStrength by remember {
-        mutableIntStateOf(-1)
-    }
-    
-    LaunchedEffect(Unit) {
-        coroutineScope.launch {
-            val signalWifiStrength = getWifiSignalStrength(context)
-            signalStrength = signalWifiStrength
-        }
-    }
-    
-    Text(
-        text = "Hello $connectionType - $signalStrength",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ConnectivityAppTheme {
-        Greeting("Android")
     }
 }
